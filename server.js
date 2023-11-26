1355e311865454fe0176b490dde4d82843b5a2b4
rcoderx <104567101+rcoderx@users.noreply.github.com>
2023-11-26 09:34:40 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
 const app = express();
 app.use(cors({
-    origin: 'https://legendary-memory-ten.vercel.app/' // Replace with your front-end's domain
+    origin: 'https://legendary-memory-ten.vercel.app' // Replace with your front-end's domain
 }));
 const corsOptions = {
     origin: 'https://legendary-memory-ten.vercel.app',
