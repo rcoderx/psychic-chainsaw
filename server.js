857129cc4959bf49601d0c2ac038e8968c32f6c5
rcoderx <104567101+rcoderx@users.noreply.github.com>
2023-11-28 07:43:20 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -185,10 +185,6 @@ async function calculateRewards() {
 
 
 
-function toSmallestTokenUnit(amount, decimals) {
-    return BigInt(amount) * BigInt(10 ** decimals);
-}
-
 async function distributeRewards(playerRewards) {
     // Ensure there are rewards to distribute
     if (playerRewards.length === 0) {
@@ -198,7 +194,7 @@ async function distributeRewards(playerRewards) {
 
     // Prepare the addresses and amounts arrays
     const addresses = playerRewards.map(reward => reward.address);
-    const amounts = playerRewards.map(reward => reward.reward);
+    const amounts = playerRewards.map(reward => toSmallestTokenUnit(reward.reward, 18)); // Convert to smallest unit using the custom function
 
     // Initialize the wallet and contract for transactions
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
@@ -206,7 +202,7 @@ async function distributeRewards(playerRewards) {
 
     try {
         // Call the distributeRewards function of the smart contract
-        const tx = await rewardContract.distributeRewards(addresses, amounts);
+        const tx = await rewardContract.distributeRewards(addresses, amounts.map(amount => amount.toString()));
         await tx.wait();
         console.log(`Rewards distributed successfully`);
 
@@ -215,8 +211,10 @@ async function distributeRewards(playerRewards) {
 
     } catch (error) {
         console.error(`Error distributing rewards:`, error);
+        // Do not clear rewards if there's an error, so you can retry
     }
 }
+
 
 
 
